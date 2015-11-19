f124256efec2c5a4b3955b839f976df96a0162c3
Monty Taylor <mordred@inaugust.com>
2015-11-19 09:51:45 -0500

    Gave this consistent scales and titles

--- src/js/task-report.js
+++ src/js/task-report.js
@@ -13,9 +13,12 @@ float_tasks = [
     'ListFloatingIPs'
 ]
 
-providers = [
+big_providers = [
     'hpcloud',
     'rax',
+]
+
+small_providers = [
     'bluebox',
     'ovh'
 ]
@@ -34,12 +37,13 @@ for(i=0; i<tasks.length; ++i) {
       bgcolor: 'ffffff',
       fgcolor: '000000',
       lineMode: 'connected',
+      vtitle: 'Time in Seconds',
       title: tasks[i],
       target: [
-          "alias(averageSeries(stats.timers.nodepool.task.hpcloud-b*." + tasks[i] + "Task.mean), 'HP')",
-          "alias(averageSeries(stats.timers.nodepool.task.bluebox-sjc1." + tasks[i] + "Task.mean), 'BB')",
-          "alias(averageSeries(stats.timers.nodepool.task.ovh-gra1." + tasks[i] + "Task.mean), 'OVH')",
-          "alias(averageSeries(stats.timers.nodepool.task.rax-*." + tasks[i] + "Task.mean), 'RAX')",
+          "alias(scale(averageSeries(stats.timers.nodepool.task.hpcloud-b*." + tasks[i] + "Task.mean), '0.001'), 'HP')",
+          "alias(scale(averageSeries(stats.timers.nodepool.task.bluebox-sjc1." + tasks[i] + "Task.mean), '0.001'), 'BB')",
+          "alias(scale(averageSeries(stats.timers.nodepool.task.ovh-gra1." + tasks[i] + "Task.mean), '0.001'), 'OVH')",
+          "alias(scale(averageSeries(stats.timers.nodepool.task.rax-*." + tasks[i] + "Task.mean), '0.001'), 'RAX')",
       ]
   }));
 
@@ -54,16 +58,18 @@ for(i=0; i<float_tasks.length; ++i) {
       bgcolor: 'ffffff',
       fgcolor: '000000',
       lineMode: 'connected',
+      vtitle: 'Time in Seconds',
+      yMax: '10',
       title: float_tasks[i],
       target: [
-          "alias(averageSeries(stats.timers.nodepool.task.hpcloud-b*." + float_tasks[i] + "Task.mean), 'HP')",
-          "alias(averageSeries(stats.timers.nodepool.task.bluebox-sjc1." + float_tasks[i] + "Task.mean), 'BB')",
+          "alias(scale(averageSeries(stats.timers.nodepool.task.hpcloud-b*." + float_tasks[i] + "Task.mean), '0.001'), 'HP')",
+          "alias(scale(averageSeries(stats.timers.nodepool.task.bluebox-sjc1." + float_tasks[i] + "Task.mean), '0.001'), 'BB')",
       ]
   }));
 
 }
 
-for(i=0; i<providers.length; ++i) {
+for(i=0; i<big_providers.length; ++i) {
   $("#graph-container").append($(new Image()).addClass('graph').graphite({
       from: "-72hours",
       width: 885,
@@ -71,10 +77,29 @@ for(i=0; i<providers.length; ++i) {
       bgcolor: 'ffffff',
       fgcolor: '000000',
       areaMode: 'stacked',
-      title: providers[i] + " nodes launched",
+      yMax: '800',
+      title: big_providers[i] + " nodes launched",
       target: [
-         "color(alias(summarize(sumSeries(stats_counts.nodepool.launch.provider." + providers[i] + "*.ready), '1h'), 'Ready'), '00ff22')",
-         "color(alias(summarize(sumSeries(stats_counts.nodepool.launch.provider." + providers[i] + "*.error.*), '1h'), 'Error'), 'ff0000')"
+         "color(alias(summarize(sumSeries(stats_counts.nodepool.launch.provider." + big_providers[i] + "*.ready), '1h'), 'Ready'), '00ff22')",
+         "color(alias(summarize(sumSeries(stats_counts.nodepool.launch.provider." + big_providers[i] + "*.error.*), '1h'), 'Error'), 'ff0000')"
+      ]
+  }));
+
+}
+
+for(i=0; i<small_providers.length; ++i) {
+  $("#graph-container").append($(new Image()).addClass('graph').graphite({
+      from: "-72hours",
+      width: 885,
+      height: 495,
+      bgcolor: 'ffffff',
+      fgcolor: '000000',
+      areaMode: 'stacked',
+      yMax: '25',
+      title: small_providers[i] + " nodes launched",
+      target: [
+         "color(alias(summarize(sumSeries(stats_counts.nodepool.launch.provider." + small_providers[i] + "*.ready), '1h'), 'Ready'), '00ff22')",
+         "color(alias(summarize(sumSeries(stats_counts.nodepool.launch.provider." + small_providers[i] + "*.error.*), '1h'), 'Error'), 'ff0000')"
       ]
   }));
 
